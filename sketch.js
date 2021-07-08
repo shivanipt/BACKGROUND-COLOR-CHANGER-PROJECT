f27634fb31fa276a23a2d3dfa71446649f1c9759
shivanipt <87143211+shivanipt@users.noreply.github.com>
2021-07-08 18:26:55 -0500

    Add files via upload
    Colour changer #2 - submitted wrong one before

--- sketch.js
+++ sketch.js
@@ -1,28 +1,36 @@
-var box;
+var btn_pink;
+var btn_blue;
 
-function setup(){
-  createCanvas(400, 400);
-  box = createSprite(200, 200, 30, 30);
+var r = 0;
+var g = 0;
+var b = 0;
+
+function setup() {
+  createCanvas(400, 400); 
+  
+  btn_pink = createButton("PINK");
+  btn_pink.position(150, 200);
+  btn_pink.mousePressed(pink_bg);
+
+  btn_blue = createButton("BLUE");
+  btn_blue.position(300, 200);
+  btn_blue.mousePressed(blue_bg);
+
 }
 
-function draw(){
-  background("white");
-
-  if(keyIsDown(RIGHT_ARROW)){
-    box.position.x = box.position.x + 5;
-  }
-
-  if(keyIsDown(LEFT_ARROW)){
-    box.position.x = box.position.x - 5;
-  }
-
-  if(keyIsDown(UP_ARROW)){
-    box.position.y = box.position.y - 5;
-  }
-
-  if(keyIsDown(DOWN_ARROW)){
-    box.position.y = box.position.y + 5;
-  }
-
-  drawSprites();
+function pink_bg(){
+  r = 91;
+  g = 59;
+  b = 95;
 }
+
+function blue_bg(){
+  r = 15;
+  g = 22;
+  b = 89;
+}
+
+function draw() {
+  background(r,g,b);
+}
+
